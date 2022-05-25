d6e3fc7900dc6901e0bd65e2b8a6e8468ee9581e
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-05-25 22:50:37 +0300

    task-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const buttonStart = document.Qeryselector("button[data-start]")
+const buttonStop = document.Qeryselector("button[data-stop]")
 
+buttonStart.addEventListener("click", onStartBtnClick)
+buttonStop.addEventListener("click", onStopBtnClick)
+
+const DELAY = 1000;
+
+
+function onStartBtnClick() {
+    intervalId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    },DELAY);
+    changeBtnStatus(false, true);
+};
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    changeBtnStatus(true, false);
+}
